666c243563f09e17c26d6cba9fa6566e3da803fa
lydia_air_25 <lydia_air_25@lydia-air-25s-MacBook-Air.local>
2025-08-18 18:41:52 +0800

    added sources

--- models/staging/src_movies.sql
+++ models/staging/src_movies.sql
@@ -1,11 +1,10 @@
 WITH raw_movies AS(
-    SELECT * FROM MOVIELENS.RAW.RAW_MOVIES -- This is the directory on SNOW 
-
+    SELECT * FROM {{ source('netflix','r_movies') }}
+--above uses the table and identifier name in sources.yml instead of the directory on SNOW
+-- This is the directory on SNOW  MOVIELENS.RAW.RAW_MOVIES
 )
-
 SELECT
     movieId AS movie_id,
     title,
     genres
 FROM raw_movies
-
