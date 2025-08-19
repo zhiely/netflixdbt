6aa5a13172e38463a45712dca94b0821f88449e4
lydia_air_25 <lydia_air_25@lydia-air-25s-MacBook-Air.local>
2025-08-19 16:23:42 +0800

    singular test case to check relevance_score is below or equal to 0 since the range should be 0 to 1

---
+++ tests/relevance_score_test.sql
@@ -0,0 +1,19 @@
+-- singular test case: this is to test fct_genome_scores's column called relevance_score  if relevance_score is less than or equal to 3. If fail, means that its less than 3.
+
+
+
+-- SELECT 
+-- 	movie_id,
+--     tag_id,
+--     relevance_score
+-- FROM {{ ref('fct_genome_scores')}}
+-- WHERE relevance_score <= 3
+-- test fail
+
+SELECT 
+	movie_id,
+    tag_id,
+    relevance_score
+FROM {{ ref('fct_genome_scores')}}
+WHERE relevance_score <= 0
+test pass
